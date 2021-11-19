7b8c55c0593d97f3837d6388febc70efb35091be
Marcelo Rodrigues Costa <capp59marcelo@hotmail.com>
2021-11-18 22:52:45 -0300

    :sparkles: Third class template method
    This class talk about template method and flake8

---
+++ pep8-type-hints/2-template_method/fila_prioritaria.py
@@ -0,0 +1,33 @@
+from typing import Dict, List
+
+from fila_base import FilaBase
+
+
+class FilaPrioritaria(FilaBase):
+    def gera_senha_atual(self) -> None:
+        self.senha_atual = f"PR{self.codigo}"
+
+    def chama_cliente(self, caixa: int) -> List[str]:
+        mostrar: list[str] = []
+        cliente_atual = self.fila.pop(0)
+        mostrar.append(f"Cliente: ]{cliente_atual} - Caixa {caixa}")
+
+        if len(self.fila) >= 3:
+            mostrar.append(f"Próximo: {self.fila[0]}")
+            mostrar.append(f"Fique atento: {self.fila[1]}")
+
+        self.clientes_atendidos.append(cliente_atual)
+
+        return mostrar
+
+    def estatistica(self, dia: str, agencia: str, flag: str) -> Dict[str, int]:
+        if flag != "detail":
+            estatistica = {f"{agencia} - {dia}": len(self.clientes_atendidos)}
+        else:
+            estatistica = {}
+            estatistica["dia"] = dia
+            estatistica["agencia"] = agencia
+            estatistica["clientes atendidos"] = self.clientes_atendidos
+            estatistica["quantidade de clientes atendidos"] = len(self.clientes_atendidos)
+
+        return estatistica
